504a8bf748c595a7c15c67658deb10642d256dde
Thuvaragan <54566564+thuvaragan-g@users.noreply.github.com>
2024-08-22 12:23:01 +0530

    Update PoddleNfcPlugin.swift

--- ios/Classes/PoddleNfcPlugin.swift
+++ ios/Classes/PoddleNfcPlugin.swift
@@ -40,14 +40,13 @@ public class PoddleNfcPlugin: NSObject, FlutterPlugin, NFCNDEFReaderSessionDeleg
        
        func ScantnAction() {
            nfcSession = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+           // Set an alert message to guide the user
+           session?.alertMessage = "Hold your iPhone near a writable NFC tag to update."
            nfcSession?.begin()
        }
        
        
     public func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
-         // Set an alert message to guide the user
-            session.alertMessage = "Hold your iPhone near a writable NFC tag to update."
-        
            var result = ""
            for payload in messages[0].records{
                result += String.init (data: payload.payload, encoding: .ascii) ?? "Format not supported"
